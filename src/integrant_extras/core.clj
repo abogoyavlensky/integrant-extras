860d7946955e6a0e7f3ddfc3841bfe6288dca0dc
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-03-09 04:05:11 +0000

    Add config and system utils

--- src/integrant_extras/core.clj
+++ src/integrant_extras/core.clj
@@ -1,6 +1,68 @@
-(ns integrant-extras.core)
+(ns integrant-extras.core
+  (:require [aero.core :as aero]
+            [clojure.java.io :as io]
+            [clojure.tools.logging :as log]
+            [integrant.core :as ig]
+            [malli.core :as m]
+            [malli.error :as me]
+            [malli.util :as mu])
+  (:import (clojure.lang IFn)
+           (java.net ServerSocket)))
 
-(defn sum
-  "Sum two numbers."
-  [a b]
-  (+ a b))
+; Config
+
+(def ^:const SYSTEM-CONFIG-PATH-DEFAULT "config.edn")
+
+; Add #ig/ref tag for reading integrant config from aero.
+(defmethod aero/reader 'ig/ref
+  [_ _ value]
+  (ig/ref value))
+
+(defmethod aero/reader 'free-port
+  [_ _ _value]
+  (with-open [socket (ServerSocket. 0)]
+    (.getLocalPort socket)))
+
+(defn get-config
+  "Return edn config with all variables set."
+  ([profile]
+   (get-config profile nil))
+  ([profile config-path]
+   (-> (or config-path SYSTEM-CONFIG-PATH-DEFAULT)
+       (io/resource)
+       (aero/read-config {:profile profile
+                          :resolver aero/resource-resolver}))))
+
+(defn validate-schema!
+  "Validate data against schema and throw a humanized error if data is not valid."
+  [{:keys [component schema data]}]
+  (some-> schema
+          (mu/closed-schema)
+          (m/explain data)
+          (me/with-spell-checking)
+          (me/humanize)
+          (#(throw (Exception. (format "Invalid %s component config: %s" component %))))))
+
+; System
+
+(defn at-shutdown
+  "Add hook for shutdown system on sigterm."
+  [system]
+  (-> (Runtime/getRuntime)
+      (.addShutdownHook
+        (Thread. ^IFn (bound-fn []
+                        (log/info "[SYSTEM] System is stopping...")
+                        (ig/halt! system)
+                        (shutdown-agents)
+                        (log/info "[SYSTEM] System has been stopped."))))))
+
+(defn run-system
+  "Run application system."
+  [{:keys [profile config-path]}]
+  (let [config (get-config profile config-path)]
+    (log/info "[SYSTEM] System is starting with profile:" profile)
+    (ig/load-namespaces config)
+    (-> config
+        (ig/init)
+        (at-shutdown))
+    (log/info "[SYSTEM] System has been started successfully.")))
---
+++ src/integrant_extras/tests.clj
@@ -0,0 +1,20 @@
+(ns integrant-extras.tests
+  (:require [integrant.core :as ig]
+            [integrant-extras.core :as ig-extras]))
+
+(def ^:dynamic *test-system* nil)
+
+(defn with-system
+  "Run the test system before tests."
+  ([]
+   (with-system nil))
+  ([config-path]
+   (fn
+     [test-fn]
+     (let [test-config (ig-extras/get-config :test config-path)]
+       (ig/load-namespaces test-config)
+       (binding [*test-system* (ig/init test-config)]
+         (try
+           (test-fn)
+           (finally
+             (ig/halt! *test-system*))))))))
